8e1330bfccef6e2247baaa07e2a4d1e9854aeb4c
MuzSh <muzamal2012@gmail.com>
2022-12-01 02:56:57 -0500

    motion

--- src/js/vidChange.js
+++ src/js/vidChange.js
@@ -10,6 +10,7 @@ function loadPage() {
     setTimeout(loadAfterTime3, 159000);
     setTimeout(loadAfterTime4, 224000);
     setTimeout(loadAfterTime5, 262000);
+    setTimeout(loadAfterTime6, 280000);
 
     };
     function loadAfterTime5(){
@@ -33,9 +34,13 @@ function loadPage() {
       video.load();
       video.play();
     };
+    function loadAfterTime6(){
+      var video = document.getElementById("bgvid");
+      video.setAttribute('src', './src/video/redeye.mp4');
+      video.load()
+      video.play();
+    };
     function loadAfterTime(){
       var video = document.getElementById("bgvid");
       video.play();
-      // code you need to execute goes here. 
     };
-    
