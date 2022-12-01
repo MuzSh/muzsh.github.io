b26dc3711403ecfa9cb301191203be2e5d145779
MuzSh <muzamal2012@gmail.com>
2022-12-01 03:52:12 -0500

    msg update

--- src/js/typewriter.js
+++ src/js/typewriter.js
@@ -3,9 +3,19 @@ var index = 0;
 var textPosition = 0; 
 var flag = true;
 
+var statements = ['print', 'system.out.println', 'console.log', " cout << "];
+var rand = Math.floor(Math.random() * statements.length);
+var endings = ['', ';', ';', ';'];
+var current 
+
+
+console.log(statements);
+console.log(endings);
+console.log(rand);
+console.log(endings[rand]);
+
 loadQuote = () => {
   const url = 'https://api.quotable.io/random';
-
   fetch(url)
 
   .then(response => {
@@ -14,7 +24,7 @@ loadQuote = () => {
    })
 
    .then(data => {
-      quoteArray[index] = data.content;
+      quoteArray[index] = statements[rand] + "('" + data.content + "')" + endings[rand];
    })
 
    .catch(error => console.log(error));
@@ -29,8 +39,9 @@ typewriter = () => {
 
   document.querySelector("#quote").innerHTML = quoteArray[index].substring(0, textPosition) + '<span>\u25AE</span>';
 
+
   if(textPosition++ != quoteArray[index].length){
-    setTimeout("typewriter()", 66.6);
+    setTimeout("typewriter()", 66.66);
   }
   else{
     quoteArray[index] = ' ';
