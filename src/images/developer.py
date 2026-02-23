4f04a4b0a6e0cce2b1a7763ff3fd23b2de915668
MuzSh <muzamal2012@gmail.com>
2026-02-22 23:05:38 -0500

    modify link to image

--- src/images/developer.py
+++ src/images/developer.py
@@ -1,14 +1,20 @@
 
-developer = {
+
+
+developer = { 
         "name": "Muzamal Sheikh",
         "aspiration": "Software Developer / Software Engineer / Full-Stack Developer",
         "interests": ["Artificial Intelligence", "Machine Learning", "Data Science & Analytics"],
         "major": "Computer Science",
-        "masters": "Data Science",
+        "masters": "Applied Data Science",
         "minor": "Mathematics",
-        "college": "Ramapo College of New Jersey",
-        "languages": ["Python", "C++", "JavaScript", "C#", "TypeScript", "Kotlin"],
-        "webDevelopment": ["React", "Node"],
-        "database": ["PostgreSQL", "MongoDB"],
-        "technologies": ["VS Code"]
-    }
+        "college": ["Ramapo College of New Jersey", "Stevens Institute of Technology"],
+        "ai_tools": ["ChatGPT", "Claude", "Gemini", "Perplexity", "n8n"],
+        "database": ["PostgreSQL", "NoSQL", "Oracle", "MongoDB"],
+        "open_source": ["Git, Github, Jupyter Notebook"],
+        "web_development": ["React", "Node", "HTML", "CSS"],
+        "coding_language": ["Python", "C++", "JavaScript", "C#", "TypeScript", "Kotlin"],
+        "software_tools": ["VS Code", "Microsoft Visual Studio"],
+        "business": ["Microsoft Excel", "PowerBI", "Tableau"]
+}
+
