0a0e143874301adb60d0d10662300a8ccdbe0a2d
MuzSh <muzamal2012@gmail.com>
2026-03-04 02:31:56 -0500

    reconfigure loading time sequence; adjusted for screensaver loading too

--- src/js/vidChange.js
+++ src/js/vidChange.js
@@ -3,46 +3,47 @@ function loadPage() {
     var audio = document.getElementById("myaudio");
     audio.load();
     audio.play();
-    audio.volume = 0.3; 
+    audio.volume = 0.3;
 
     setTimeout(loadAfterTime, 31333);
     setTimeout(loadAfterTime2, 63000);
     setTimeout(loadAfterTime3, 159000);
     setTimeout(loadAfterTime4, 224000);
     setTimeout(loadAfterTime5, 262000);
-    setTimeout(loadAfterTime6, 280000);
+    setTimeout(loadAfterTime6, 270000);
+    setTimeout(loadScreensaver,270420); // triggers screensaver
+  };
 
-    };
-    function loadAfterTime5(){
-      document.querySelector('bgvid').defaultPlaybackRate = 0.25;
-    };
-    function loadAfterTime4(){
-      var video = document.getElementById('bgvid');
-      video.setAttribute('src', './src/video/inf.mp4');
-      video.load();
-      video.play();
-    };
-    function loadAfterTime3(){
-      var video = document.getElementById('bgvid');
-      video.setAttribute('src', './src/video/glb.mp4');
-      video.load();
-      video.play();
-    };
-    function loadAfterTime2(){
-      var video = document.getElementById('bgvid');
-      video.setAttribute('src', './src/video/redeye2.mp4');
-      video.load();
-      video.play();
-    };
-    function loadAfterTime6(){
-      var video = document.getElementById("bgvid");
-      video.setAttribute('src', './src/video/redeye.mp4');
-      video.load()
-      video.play();
-    };
-    // load and last and pause
-    function loadAfterTime(){
-      var video = document.getElementById("bgvid");
-      video.play();
-      video.stop();
-    };
+
+function loadAfterTime5() {
+    document.querySelector('bgvid').defaultPlaybackRate = 0.25;
+};
+function loadAfterTime4(){
+    var video = document.getElementById('bgvid');
+    video.setAttribute('src', './src/video/inf.mp4');
+    video.load();
+    video.play();
+};
+function loadAfterTime3(){
+    var video = document.getElementById('bgvid');
+    video.setAttribute('src', './src/video/glb.mp4');
+    video.load();
+    video.play();
+};
+function loadAfterTime2(){
+    var video = document.getElementById('bgvid');
+    video.setAttribute('src', './src/video/redeye2.mp4');
+    video.load();
+    video.play();
+};
+function loadAfterTime6(){
+    var video = document.getElementById("bgvid");
+    video.setAttribute('src', './src/video/redeye.mp4');
+    video.load();
+    video.play();
+};
+function loadAfterTime(){
+    var video = document.getElementById("bgvid");
+    video.play();
+    video.stop();
+};
